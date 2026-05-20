bee7e6f98383b983f20aaa455dd667431c4792d8
Muhammad Mehran <mehranjunjua@gmail.com>
2026-05-20 06:54:44 +0500

    first commit

---
+++ core.py
@@ -0,0 +1,20 @@
+
+print("Hello World!")
+name = "Ali"
+city = "Islamabad"
+print(name)
+print(city)
+full_name = name + " " + city
+print(full_name)
+print(len(full_name))
+age = 29
+hight = 5.11 
+print(age , hight)
+age = input("Enter your age: ")
+chest = int(input("Enter your chest: "))
+print(bool(chest))
+
+
+List = [name, age, hight]
+print(List)
+
---
+++ logic.py
@@ -0,0 +1,24 @@
+age = 16
+if age >= 18:
+    print("Adult")
+
+for i in range(4):
+    print("junjua sab tension not")
+
+city = ["lahore", "pindi"]
+
+for cites in city:
+    print(cites)
+    
+for a in range(2, 20, 3):
+    print(a)
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count = count + 1
+
+for i in  range(6):
+    for j in range (3):
+        print("junjua sab tension nai lani")
